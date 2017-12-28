53bc8d38e0b4050840469299aeea16e4d50248b0
Akshit Grover <akshit.grover2016@gmail.com>
2017-12-29 02:45:42 +0530

    Add Arithmetic Operators

---
+++ arithmeticops.go
@@ -0,0 +1,33 @@
+package main
+
+func main() {
+	add := 7 + 9 + 11
+
+	sub := 11 - 7
+
+	div := 9 / 3
+
+	mul := 7 * 3
+
+	rem := 11 % 9
+
+	println(add)
+	println(sub)
+	println(div)
+	println(mul)
+	println(rem)
+
+	inc := 7
+
+	println(inc)
+
+	inc++
+	inc++
+
+	println(inc)
+
+	inc--
+	inc--
+
+	println(inc)
+}
